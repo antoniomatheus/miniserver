4f734f15813e836557cd8f2bd614fbf92fed6eec
Antonio Matheus <antonimathsilva@gmail.com>
2020-02-22 10:47:22 -0300

    multithread server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,22 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::net::TcpListener;
-use std::thread;
-use std::time::Duration;
 use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::time::Duration;
+use std::thread;
+
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -33,7 +38,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     let contents = fs::read_to_string(filename).unwrap();
     let response = format!("{}{}", status_line, contents);
-    
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    /// 
+    /// The size is the number of threads in the pool.
+    /// 
+    /// # Panics
+    /// 
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+        
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 1..size + 1 {
+            let worker = Worker::new(id, Arc::clone(&receiver));
+
+            workers.push(worker);
+        }
+
+        ThreadPool {
+            workers,
+            sender
+        }
+    }
+
+    pub fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+
+                job();
+            }
+        });
+
+        Worker {
+            id,
+            thread,
+        }
+    }
+}
+
